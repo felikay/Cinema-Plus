49714e2e830fc7f8b31df40a639c5d192136b804
Felicia Kyalo <feliciakyalo@gmail.com>
2023-12-04 17:15:45 +0300

    completed payment page

--- app/src/main/java/com/example/cinemaplus/payment/payment.kt
+++ app/src/main/java/com/example/cinemaplus/payment/payment.kt
@@ -14,122 +14,102 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.cinemaplus.MainActivity
 import com.example.cinemaplus.R
-import com.example.cinemaplus.home.Main
-import com.example.cinemaplus.showpage.MoviePage
 
-class Payment : ComponentActivity() {
+class PaymentActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PaymentScreen(
-                navigateToMain = { navigateToMain() }
+            PaymentScreen {
+                val homeIntent = Intent(this, MainActivity::class.java)
+                homeIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(homeIntent)
+                finish()
+            }
+        }
+    }
+}
+
+@Composable
+fun PaymentScreen(onBackToHomeClicked: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFF133755)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Payment",
+                color = Color.White,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 32.dp, bottom = 16.dp)
             )
+            // Detail box with a white background
+            Surface(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                color = Color.White,
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "BARBIE",
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        "Ksh. 1200",
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        "Saturday 13:00\n29/11/2023",
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.barcode),
+                        contentDescription = "Barcode",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp) // Adjust the size as needed
+                    )
+                    Text(
+                        "9876437459693",
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = onBackToHomeClicked,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp, vertical = 16.dp)
+                    .height(48.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0B1F30)),
+                shape = RoundedCornerShape(50)
+            ) {
+                Text("Back to home", color = Color.White, fontSize = 18.sp)
+            }
         }
     }
-
-    private fun navigateToMain() {
-        val intent = Intent(this, Main::class.java)
-        startActivity(intent)
-    }
-}
-
-
-@Composable
-fun PaymentScreen(navigateToMain: () -> Unit) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color(0xFF133755)
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            TicketCard()
-            BackToHomeButton(onBack = navigateToMain)
-        }
-    }
-}
-
-@Composable
-fun TicketCard() {
-    Card(
-        shape = RoundedCornerShape(16.dp),
-        modifier = Modifier
-            .padding(horizontal = 24.dp, vertical = 64.dp)
-            .fillMaxWidth(),
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .background(Color.White)
-                .padding(16.dp)
-        ) {
-            Text("BARBIE", fontSize = 24.sp, color = Color.Black, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("Ksh. 1200", fontSize = 18.sp, color = Color.Gray, textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("Monday 18:30", fontSize = 18.sp, color = Color.Gray, textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("17/11/2023", fontSize = 18.sp, color = Color.Gray, textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.height(24.dp))
-            Divider(color = Color.Black, thickness = 2.dp)
-            Spacer(modifier = Modifier.height(24.dp))
-            BarcodeView()
-        }
-    }
-}
-
-@Composable
-fun BarcodeView() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.barcode),
-            contentDescription = "Barcode",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 32.dp)
-                .height(100.dp)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            "9876437459963",
-            color = Color.Black,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
-}
-
-@Composable
-fun BackToHomeButton(onBack: () -> Unit) {
-    Button(
-        onClick = onBack,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .height(48.dp),
-        shape = RoundedCornerShape(50),
-        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1f2a44))
-    ) {
-        Text("Back to home", color = Color.White, fontSize = 18.sp)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewPaymentScreen() {
-    PaymentScreen(
-        navigateToMain = {}
-    )
 }
