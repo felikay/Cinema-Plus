815780b7a684248793c62d305edabd4cecb2168d
Felicia Kyalo <feliciakyalo@gmail.com>
2023-12-04 17:16:42 +0300

    updated files

--- app/src/main/java/com/example/cinemaplus/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cinemaplus.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val ColumbiaBlue = Color(0xFFBAD8E7)
-val OxfordBlue = Color(0xFF02162C)
-val AliceBlue = Color(0xFFD9E9F1)
-val VividSkyBlue = Color(0xFF02D7FC)
-val PrussianBlue = Color(0xFF133755)
